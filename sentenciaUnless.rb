6f9b4d137e43d38859ec197c49562e61a2caaac7
manu9812 <32127007+manu9812@users.noreply.github.com>
2018-09-19 22:59:24 -0500

    sentencia unless

---
+++ sentenciaUnless.rb
@@ -0,0 +1,13 @@
+class sentencias
+  def initialize()
+  end
+  def saludar()
+    bloqueado ="juan"
+    unless bloqueado =="juan"
+      print"bienvenido a la fiesta "
+    end
+  end
+end
+objeto= sentencias.new()
+objeto.saludar
+gets()
