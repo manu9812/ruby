4e9a46a70cafe176353f9a8bc44db27a950f438e
manu9812 <32127007+manu9812@users.noreply.github.com>
2018-09-20 08:56:32 -0500

    ciclo while ruby

---
+++ CicloWhile.rb
@@ -0,0 +1,26 @@
+class Ciclos
+  def initialize()
+  end
+    def ciclicos()
+      i=0
+      begin
+        print i
+        i+=1
+      end while i <5
+
+  end
+end
+
+=begin
+i=0
+until i>5 do
+  print i
+  +=1
+
+end
+
+
+i=0
+begin
+end untin # i >5
+=end
